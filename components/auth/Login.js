3e30b13b6912cd379e3a90ed54792e2c0af62651
iphyviktor <victormengwaiphy@gmail.com>
2022-06-02 17:39:41 +0100

    add style to login and register

--- components/auth/Login.js
+++ components/auth/Login.js
@@ -1,6 +1,7 @@
 import { useState, useEffect, useContext } from 'react'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import AuthContext from '../../context/AuthContext'
 import { toast } from 'react-toastify'
@@ -29,31 +30,55 @@ const Login = () => {
 
   return (
     <div className='flex h-screen w-full'>
-        <div className="w-full h-full bg-contain">Test</div>
-        <div>
-        <h1>Login</h1>
-        <form onSubmit={submitHandler}>
-            <input type='text' 
-            placeholder='Username' 
-            required 
-            value={username} 
-            onChange={(e) => setUsername(e.target.value)} 
-            // pattern ='\S+@\S+\.\S+'
-            // title='Your username is invalid'
-            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
-            />
-            <br/>
-            <input type={`password`} 
-            placeholder='Password' 
-            required 
-            value={password} onChange={(e) => setPassword(e.target.value)} 
-            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600 mt-4"
-            />
-            <br/>
-            <button type='submit' className='bg-red-600 hover:bg-red-400 duration-500 text-white py-2 px-6 rounded-sm text-sm mt-4'>
-                {loading ? 'Authenticating...' : 'Login'}
-            </button>
-        </form>
+        <div className="w-3/5 h-full bg-auth-bg-img bg-no-repeat bg-cover bg-center">
+        <div className='m-auto text-center self-center'>
+                <h1 className='text-4xl text-white font-bold'>Ethnos Vulnerability Scanner</h1>
+                <p className='text-white font-bold'>Using up-to-date CVEs, gain insight into the <br/>security posture of your infrastructure</p>
+            </div>   
+        </div>
+
+
+        <div className='w-2/5 m-auto items-center px-10'>
+            <div className='mb-10'>
+             <Image src={`/images/logo.png`} width="120" height="40" alt='logo'/>
+            </div>
+            
+            <div className=''>
+                <h1 className='text-2xl font-bold'>Log in</h1>
+                {/* <p className='text-lg'>Get into your account</p> */}
+            </div>
+        
+        <div className='w-full mt-12'>
+            <form onSubmit={submitHandler}>
+                <input type='text' 
+                placeholder='Username' 
+                required 
+                value={username} 
+                onChange={(e) => setUsername(e.target.value)} 
+                // pattern ='\S+@\S+\.\S+'
+                // title='Your username is invalid'
+                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+                />
+                <br/>
+                <input type={`password`} 
+                placeholder='Password' 
+                required 
+                value={password} onChange={(e) => setPassword(e.target.value)} 
+                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600 mt-4"
+                />
+                <br/>
+                <button type='submit' className='bg-red-600 hover:bg-red-400 duration-500 text-white py-2 px-6 rounded-sm text-sm mt-4'>
+                    {loading ? 'Authenticating...' : 'Login'}
+                </button>
+            </form>
+
+            <div className='text-center mt-5'>
+                <p>Need an account?</p>
+                <Link href="/register">
+                    <a className='hover:text-red-500 font-bold text-sm'>Signup here</a>
+                </Link>
+            </div>
+        </div>
         </div>
         
     </div>
--- components/auth/Register.js
+++ components/auth/Register.js
@@ -1,6 +1,7 @@
 import { useState, useEffect, useContext } from 'react'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import AuthContext from '../../context/AuthContext'
 import { toast } from 'react-toastify'
@@ -31,40 +32,64 @@ const Register = () => {
     }
 
   return (
-    <div>
-        <h1>Signup</h1>
-        <form onSubmit={submitHandler}>
-            <input type='text' 
-            placeholder='Username' 
-            required 
-            value={username} 
-            onChange={(e) => setUsername(e.target.value)} 
-            // pattern ='\S+@\S+\.\S+'
-            // title='Your username is invalid'
-            />
-            <br/>
+    <div className='flex h-screen w-full'>
+        <div className="w-3/5 h-full bg-auth-bg-img bg-no-repeat bg-cover bg-center">
+            <div className='m-auto text-center self-center'>
+                <h1 className='text-4xl text-white font-bold'>Ethnos Vulnerability Scanner</h1>
+                <p className='text-white font-bold'>Using up-to-date CVEs, gain insight into the <br/>security posture of your infrastructure</p>
+            </div>   
+        </div>
 
-            <input type='text' 
-            placeholder='email' 
-            required 
-            value={email} 
-            onChange={(e) => setEmail(e.target.value)} 
-            pattern ='\S+@\S+\.\S+'
-            title='Your email is invalid'
-            />
-            <br/>
+        <div className='w-2/5 m-auto items-center px-10'>
+            <div className='mb-10'>
+                <Image src={`/images/logo.png`} width="120" height="40" alt='logo'/>
+            </div>
+            <h1 className='text-2xl font-bold'>Signup</h1>
+            <div className='w-full mt-12'>
+                <form onSubmit={submitHandler}>
+                    <input type='text' 
+                    placeholder='Username' 
+                    required 
+                    value={username} 
+                    onChange={(e) => setUsername(e.target.value)} 
+                    // pattern ='\S+@\S+\.\S+'
+                    // title='Your username is invalid'
+                    className="w-full px-4 mb-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+                    />
+                    <br/>
 
-            <input type={`password`} 
-            placeholder='Password' 
-            required 
-            value={password} onChange={(e) => setPassword(e.target.value)} 
-            minLength={8}
-            />
-            <br/>
-            <button type='submit'>
-                {loading ? 'Loading...' : 'Register'}
-            </button>
-        </form>
+                    <input type='text' 
+                    placeholder='email' 
+                    required 
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)} 
+                    pattern ='\S+@\S+\.\S+'
+                    title='Your email is invalid'
+                    className="w-full px-4 py-2 mb-4 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+                    />
+                    <br/>
+
+                    <input type={`password`} 
+                    placeholder='Password' 
+                    required 
+                    value={password} onChange={(e) => setPassword(e.target.value)} 
+                    minLength={8}
+                    className="w-full px-4 py-2 mb-4 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+                    />
+                    <br/>
+                    <button type='submit' className='bg-red-600 hover:bg-red-400 duration-500 text-white py-2 px-6 rounded-sm text-sm mt-4'>
+                        {loading ? 'Loading...' : 'Register'}
+                    </button>
+                </form>
+
+                <div className='text-center mt-5'>
+                    <p>Already have an account?</p>
+                    <Link href="/login">
+                        <a className='hover:text-red-500 font-bold text-sm'>Log in here</a>
+                    </Link>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
